f7585ff178ddb19865323e288002f3eb6ff288f1
Your Name <your.email@example.com>
2024-10-04 18:07:15 +0300

    finish app

---
+++ lib/domain/usecases/get_cars.dart
@@ -0,0 +1,13 @@
+
+import '../../data/models/car.dart';
+import '../repositories/car_repository.dart';
+
+class GetCars {
+  final CarRepository repository;
+
+  GetCars(this.repository);
+
+  Future<List<Car>> call() async {
+    return await repository.fetchCars();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:car_rental_app/presentation/bloc/car_bloc.dart';
+import 'package:car_rental_app/presentation/bloc/car_event.dart';
+import 'package:car_rental_app/presentation/pages/onboarding_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'firebase_options.dart';
+import 'injection_container.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+  );
+  initInjection();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => getIt<CarBloc>()..add(LoadCars()),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: OnboardingPage(),
+      ),
+    );
+  }
+}
